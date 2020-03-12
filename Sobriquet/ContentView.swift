11fe619a41193559035491d95cda16b2779f45cf
Brandon Sorensen <sorensen.12@gmail.com>
2020-03-12 11:39:33 +0100

    Initial Commit

---
+++ Sobriquet/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Sobriquet
+//
+//  Created by Brandon Sorensen on 3/12/20.
+//  Copyright © 2020 Brandon Sorensen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
