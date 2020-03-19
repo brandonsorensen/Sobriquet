4566592cd7fef16215c659136269acd0d3a64a89
Brandon Sorensen <sorensen.12@gmail.com>
2020-03-19 10:09:22 +0100

    Changed default window size.

--- Sobriquet/AppDelegate.swift
+++ Sobriquet/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         // Create the window and set the content view. 
         window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            contentRect: NSRect(x: 0, y: 0, width: 880, height: 600),
             styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
             backing: .buffered, defer: false)
         window.center()
