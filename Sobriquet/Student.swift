75d68d8fd352926d6def238cc47f45faec90dc93
Brandon Sorensen <sorensen.12@gmail.com>
2020-03-24 09:44:08 +0100

    Added documentation.

--- Sobriquet/Student.swift
+++ Sobriquet/Student.swift
@@ -10,6 +10,7 @@ import Foundation
 import SwiftUI
 import CoreData
 
+/// Creates a `Student` object and saves it to Core Data persistent storage.
 public func addStudent(eduid: Int, lastName: String, firstName: String, middleName: String?) {
     let appDelegate = NSApplication.shared.delegate as! AppDelegate
     let managedObjectContext = appDelegate.persistentContainer.viewContext
@@ -28,11 +29,23 @@ public func addStudent(eduid: Int, lastName: String, firstName: String, middleNa
     }
 }
 
+/// Creates a `Student` object and saves it to Core Data persistent storage.
 public func addStudent(entry: CSVFields) {
     return addStudent(eduid: entry.eduid, lastName: entry.lastName,
                       firstName: entry.firstName, middleName: entry.middleName)
 }
 
+
+/**
+ 👨🏻‍🎓 A student object in the data base.
+ 
+ - Parameters:
+    - eduid: tnique student identifying ID
+    - lastName: the student's surname
+    - firstName: the student's given name
+    - middleName: the student's middle name
+    - dateAdded: the date the student was added to the data base
+ */
 public class Student: NSManagedObject, Identifiable {
 
     @NSManaged public var eduid: Int
@@ -41,6 +54,12 @@ public class Student: NSManagedObject, Identifiable {
     @NSManaged public var middleName: String?
     @NSManaged public var dateAdded: Date
     
+    /**
+     Creates a fetch request for every student in the data base sorted by their
+     first and then last name.
+     
+     - Returns: A n `NSFetchRequest` for all students in the data base.
+     */
     static func getAllStudents() -> NSFetchRequest<Student> {
         let request: NSFetchRequest<Student> = NSFetchRequest(entityName: "Student")
         
@@ -52,6 +71,11 @@ public class Student: NSManagedObject, Identifiable {
         return request
     }
     
+    /**
+        Deletes all the students in the data base.
+     
+     - Throws: An error if anything went wrong executing the batch deletion
+     */
     static func deleteAllStudents() throws {
         let fetchRequest: NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
         let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
@@ -61,6 +85,11 @@ public class Student: NSManagedObject, Identifiable {
         try managedObjectContext.executeAndMergeChanges(using: batchDeleteRequest)
     }
     
+    /**
+     Gets the time at which the last student was submitted to the data base.
+     
+     - Returns: A `Date` object represents the most recent commit to the data base
+     */
     static func getMostRecentDate() -> Date {
         let appDelegate = (NSApplication.shared.delegate) as! AppDelegate
         let moc = appDelegate.persistentContainer.viewContext
@@ -75,18 +104,38 @@ public class Student: NSManagedObject, Identifiable {
     }
 }
 
+
+/// A struct that facilitates the creation and management of `Student` objects.
 public struct StudentManager {
     
+    /// Errors that may occur in the creation or management of `Students`.
     public enum StudentManagerError: Error {
+        /// Two or more of the students have the same EDUID.
         case NonUniqueElementError
     }
     
+    /// A list of all the students
     private var allStudents: [Student]
+    
+    /// A map of all student EDUIDs to their respective students' indices in `allStudents`
     private var eduid2StudentIndex: Dictionary<Int, Int>
     
+    /// Returns the nuber of students
     public var count: Int { return allStudents.count }
+    
+    /// Whether there are any students
     public var isEmpty: Bool { return allStudents.isEmpty }
     
+    /**
+     Initializes a new `StudentManager` object by fetching all `Student`s from
+     Core Data persistent storage.
+     
+     - Throws:
+        - Any error resulting from `managedObjectContext.fetch`.
+        - `StudentManagerError.NonUniqueElementError` if
+            two or more students have the same EDUID (should be impossible)
+     - Returns: A new `StudentManager` object
+     */
     public init() throws {
         let appDelegate = (NSApplication.shared.delegate as! AppDelegate)
         let moc = appDelegate.persistentContainer.viewContext
@@ -103,6 +152,14 @@ public struct StudentManager {
         }
     }
     
+    /**
+     Creates a new `StudentManager` object from a list of students.
+     
+     - Throws:`StudentManagerError.NonUniqueElementError` if
+     two or more students have the same EDUID
+     
+     - Returns: A new `StudentManager` object
+     */
     public init(students: [Student]) throws {
         allStudents = students
         eduid2StudentIndex = StudentManager.getMapFromStudents(students: students)
@@ -112,6 +169,12 @@ public struct StudentManager {
         }
     }
     
+    /**
+     Given a file name with an EDUID, retrieves the student to whom that EDUID belongs if
+     one is found.
+     
+     - Returns: the student matching the EDUID in the file name if one exists.
+     */
     public func getStudentFromFileName(fileName: String) -> Student? {
         if let eduid: Int = StudentManager.getEduidFromString(s: fileName) {
             return getStudentForEduid(eduid: eduid)
@@ -119,6 +182,14 @@ public struct StudentManager {
         return nil
     }
     
+    /**
+     Finds an EDUID in a given string, and returns it as an `Int`if it exists.
+     
+     - Parameters:
+         - s: the given string
+     
+     - Returns: the EDUID in a string if one is found
+     */
     public static func getEduidFromString(s: String) -> Int? {
         if let eduidRange = s.range(of: #"[0-9]{7,9}"#, options: .regularExpression) {
             return Int(s[eduidRange])
@@ -126,6 +197,14 @@ public struct StudentManager {
         return nil
     }
     
+    /**
+     Returns the student for a given EDUID.
+     
+     - Parameters:
+         - eduid: the EDUID of a student in the data base
+     
+     - Returns: a `Student` object from the data base with the corresponding EDUID
+     */
     public func getStudentForEduid(eduid: Int) -> Student? {
         if let eduid = eduid2StudentIndex[eduid] {
             return allStudents[eduid]
@@ -133,7 +212,9 @@ public struct StudentManager {
         return nil
     }
     
+    /// Adds a student to the data base.
     public mutating func addStudent(student: Student) {
+        // TODO: Update the database
         if getStudentForEduid(eduid: student.eduid) != nil {
             let index = eduid2StudentIndex[student.eduid]!
             allStudents[index] = student
@@ -143,6 +224,7 @@ public struct StudentManager {
         }
     }
     
+    /// Updates the data base to match the contents of a given `Student` array
     public mutating func update(students: [Student]) throws {
         allStudents = students
         eduid2StudentIndex = StudentManager.getMapFromStudents(students: students)
@@ -152,10 +234,20 @@ public struct StudentManager {
         }
     }
     
+    /// Returns all students as an array of `Student` objects.
     public func getAllStudents() -> [Student] {
         return allStudents
     }
     
+    /**
+     Given an array of `Student` objects, creates a `Dictionary` object that maps
+     each student's EDUID to their position in the internal array.
+     
+     - Parameters:
+         - students: an array of `Student` objects
+     
+     - Returns: a map of EDUIDs to student indices in the internal array
+     */
     private static func getMapFromStudents(students: [Student]) -> Dictionary<Int, Int> {
         var map = Dictionary<Int, Int>()
         for (index, student) in students.enumerated() {
@@ -164,6 +256,7 @@ public struct StudentManager {
         return map
     }
     
+    /// Creates a map of EDUIDs to their corresponding `Student` objects.
     public func getStudentMap() -> Dictionary<Int, Student> {
         var map = Dictionary<Int, Student>()
         
@@ -174,10 +267,12 @@ public struct StudentManager {
         return map
     }
     
+    /// Clears all the students from the data base.
     public mutating func clearAll() {
         try! self.update(students: [Student]())
     }
     
+    /// Ensures that all elements in the internal array are unique.
     public func ensureUnique() -> Bool {
         var eduidSet = Set<Int>()
         for student in allStudents {
@@ -189,6 +284,7 @@ public struct StudentManager {
         return true
     }
     
+    /// Gets a student at an index location in the internal array.
     public func atIndex(index: Int) -> Student {
         return self.allStudents[index]
     }
